6a3c11ea66a664c54a337992df17e9bb723fea4a
azzizulhakim810 <azzizul.hakim202@gmail.com>
2024-05-08 16:15:11 +0600

    Different types of array

---
+++ module 1/dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var course = "Apollo Next Level Web Developement";
+console.log(course);
+// Basic Data Type
+// String
+var firstName = "Azzizul";
+// Number
+var roll = 191547;
+// Boolean
+var married = false;
+// Undefined
+var B = undefined;
+// Null
+var A = null;
+// My term to understand (Post defined)
+var C;
+C = 1234;
+// Array
+var friends = ["Ali", "Ajoy", "Shourov", "Masud"];
+var eligibleRollList = [4, 7, 8];
+eligibleRollList.push(5);
+console.log(eligibleRollList);
--- module 1/src/index.ts
+++ module 1/src/index.ts
@@ -1,2 +1,37 @@
 let course: string = "Apollo Next Level Web Developement";
 console.log(course);
+
+// Basic Data Type
+
+// String
+let firstName: string = "Azzizul";
+
+// Number
+let roll: number = 191547;
+
+// Boolean
+let married: boolean = false;
+
+// Undefined
+let B: undefined = undefined;
+
+// Null
+let A: null = null;
+
+// My term to understand (Post defined)
+let C: number;
+C = 1234;
+
+// Array
+let friends: string[] = ["Ali", "Ajoy", "Shourov", "Masud"];
+
+let eligibleRollList: number[] = [4, 7, 8];
+
+eligibleRollList.push(5);
+console.log(eligibleRollList);
+
+// tuple(Speacial type of array) --> array --> order --> type of values
+
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string, boolean] = [50, "Jim", true];
