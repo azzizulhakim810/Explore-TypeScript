7c4da8abf67123b7484ba9e02706e8f3c8cbf6df
azzizulhakim810 <azzizul.hakim202@gmail.com>
2024-08-12 15:35:59 +0600

    Working on the module 3.8

--- module_3/src/3.7.ts
+++ module_3/src/3.7.ts
@@ -1,8 +1,19 @@
 {
   // Statics
 
+  /*   class Counter {
+    static count: number = 0;
+
+    increment() {
+      return (this.count = this.count + 1);
+    }
+    decrement() {
+      return (this.count = this.count - 1);
+    }
+  } */
+
   class Counter {
-    static count: number = 0; // when we use static before a variable or method, this. replaced by the class name for the premiumness of "static" keyword usage
+    static count: number = 0; // when we use static before a variable or method, <this.> replaced by the class name <Counter> for the premiumness of "static" keyword usage
 
     static increment() {
       return (Counter.count = Counter.count + 1);
@@ -11,18 +22,20 @@
       return (Counter.count = Counter.count - 1);
     }
   }
+  /* 
+  const instance1 = new Counter();
+  console.log(Counter.increment());
 
-  // const instance1 = new Counter();
-  // console.log(instance1.increment());
+  const instance2 = new Counter();
+  console.log(Counter.increment()); */
 
-  // const instance1 = new Counter(); // don't need to create any instace, we can use static before a method as well
-  // console.log(Counter.increment());
+  const instance1 = new Counter();
+  // As we made the method "Static", so we  have to put the class name before calling the method
+  console.log(Counter.increment());
 
-  // const instance2 = new Counter();
-  // console.log(Counter.increment());
+  const instance2 = new Counter();
+  console.log(Counter.increment());
 
-  // const instance3 = new Counter();
-  // console.log(Counter.increment());
-
-  /////////
+  const instance3 = new Counter();
+  console.log(Counter.increment());
 }
