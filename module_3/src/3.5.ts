59ba1a79da7d3522ad1821c8d17566a34aaab168
azzizulhakim810 <azzizul.hakim202@gmail.com>
2024-08-31 03:31:37 +0600

    Adding Adding More & more 3

--- module_3/src/3.5.ts
+++ module_3/src/3.5.ts
@@ -29,7 +29,7 @@
     }
   }
 
-  const poorAccount = new BankAccount(1, "Mr. Jim", 100);
+  const poorAccount = new BankAccount(1, "Mr. Jim", 500);
   poorAccount.addDeposit(1000);
   const myBalace = poorAccount.getBalance();
   console.log(myBalace);
